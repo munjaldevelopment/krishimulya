67ad1285086f99135f5133324a326d63ae3893c1
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-16 19:14:54 +0530

    soft delete

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,11 +4,13 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
     use CrudTrait;
-
+    use SoftDeletes;
+    
     /*
     |--------------------------------------------------------------------------
     | GLOBAL VARIABLES
