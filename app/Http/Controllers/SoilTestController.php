04ec116e4286bb5535f1b3690cfb9dd1ccb17223
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-22 12:17:06 +0530

    Update SoilTestController.php

--- app/Http/Controllers/SoilTestController.php
+++ app/Http/Controllers/SoilTestController.php
@@ -108,7 +108,7 @@ class SoilTestController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://soil.krishitantra.com/api/farmers?contact_no=9887501240',
+        CURLOPT_URL => 'https://soil.krishitantra.com/api/farmers?contact_no=9887501241',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
