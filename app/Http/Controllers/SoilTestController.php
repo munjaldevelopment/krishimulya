7d599a45d55b166568f127e21107db23949ad1c5
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-22 17:38:07 +0530

    Update SoilTestController.php

--- app/Http/Controllers/SoilTestController.php
+++ app/Http/Controllers/SoilTestController.php
@@ -212,9 +212,9 @@ class SoilTestController extends Controller
         
         $result = json_decode($response, 1);
 
-        if(isset($result[1]))
+        if(isset($result[0]))
         {
-            echo $result[1]['uuid'];
+            echo $result[0]['uuid'];
         }
         else
         {
