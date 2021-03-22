e7d3210c8b9302932277fea377cde05d651ef046
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-22 17:45:57 +0530

    Update SoilTestController.php

--- app/Http/Controllers/SoilTestController.php
+++ app/Http/Controllers/SoilTestController.php
@@ -66,12 +66,12 @@ class SoilTestController extends Controller
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'POST',
           CURLOPT_POSTFIELDS =>'{
-            "username":"truefriend84",
-            "password":"truefriend@1234",
-            "confirm-password":"truefriend@1234",
-            "email":"truefriend84@gmail.com",
+            "username":"truefriend83",
+            "password":"truefriend83@1234",
+            "confirm-password":"truefriend83@1234",
+            "email":"truefriend83@gmail.com",
             "address":"D\\nDFDD",
-            "contact_no":"9887501240",
+            "contact_no":"9899392988",
             "longitude":28.98059049790038,
             "latitude":41.00519337980453,
             "name":"True Friend 83"
@@ -108,7 +108,7 @@ class SoilTestController extends Controller
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://soil.krishitantra.com/api/farmers?contact_no=9887501240',
+        CURLOPT_URL => 'https://soil.krishitantra.com/api/farmers?contact_no=9899392988',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
