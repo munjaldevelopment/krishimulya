0b10333980cfbecd74579a6f804bd7bc53d35291
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-23 15:37:15 +0530

    Update apiController.php

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -3803,8 +3803,6 @@ class apiController extends Controller
     {
         try 
         {
-            dd($request->all());
-            
             $json = $tractorSaleData = array();
             $date   = date('Y-m-d H:i:s');
             $customer_id = $request->customer_id;
