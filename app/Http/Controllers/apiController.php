acab014e7a56e97487b8ca42d3086c07f72328a4
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-23 14:51:54 +0530

    Update apiController.php

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -3378,11 +3378,9 @@ class apiController extends Controller
                 {
                     $tractorSellEnquiry = DB::table('tractor_purchase_enquiry')->where('customer_id', '=', $customer_id)->where('id', '=', $tractor_purchase_id)->where('isactive', '1')->first();
 
-                    $tractorPurchaseData = array('id' => "".$tractorSellEnquiry->id, 'name' => $tractorSellEnquiry->name, 'mobile' => $tractorSellEnquiry->mobile, 'company_name' => $tractorSellEnquiry->company_name, 'other_company' => ($tractorSellEnquiry->other_company == NULL ? "" : $tractorSellEnquiry->other_company), 'location' => $tractorSellEnquiry->location, 'other_city' => ($tractorSellEnquiry->other_city ==  NULL ? "" : $tractorSellEnquiry->other_city), 'hourse_power' => $tractorSellEnquiry->hourse_power, 'payment_type' => $tractorSellEnquiry->payment_type, 'comment' => ($tractorSellEnquiry->comment == NULL ? "" : $tractorSellEnquiry->comment), 'uses_type' => $tractorSellEnquiry->uses_type, 'user_type' => $tractorSellEnquiry->user_type, 'is_edit' => "".$tractorSellEnquiry->is_edit);
-
                     $status_code = '1';
                     $message = 'Tractor Puchase history';
-                    $json = array('status_code' => $status_code, 'message' => $message, 'tractorPurchaseData' => $tractorPurchaseData);
+                    $json = array('status_code' => $status_code, 'message' => $message, 'id' => "".$tractorSellEnquiry->id, 'name' => $tractorSellEnquiry->name, 'mobile' => $tractorSellEnquiry->mobile, 'company_name' => $tractorSellEnquiry->company_name, 'other_company' => ($tractorSellEnquiry->other_company == NULL ? "" : $tractorSellEnquiry->other_company), 'location' => $tractorSellEnquiry->location, 'other_city' => ($tractorSellEnquiry->other_city ==  NULL ? "" : $tractorSellEnquiry->other_city), 'hourse_power' => $tractorSellEnquiry->hourse_power, 'payment_type' => $tractorSellEnquiry->payment_type, 'comment' => ($tractorSellEnquiry->comment == NULL ? "" : $tractorSellEnquiry->comment), 'uses_type' => $tractorSellEnquiry->uses_type, 'user_type' => $tractorSellEnquiry->user_type, 'is_edit' => "".$tractorSellEnquiry->is_edit);
                 }
                 else
                 {
@@ -3423,11 +3421,9 @@ class apiController extends Controller
                 {
                     $tractorSellEnquiry = DB::table('tractor_rent_enquiry')->where('customer_id', '=', $customer_id)->where('id', '=', $tractor_rent_id)->where('isactive', '1')->first();
 
-                    $tractorRentData[] = array('id' => "".$tractorSellEnquiry->id, 'name' => $tractorSellEnquiry->name, 'mobile' => $tractorSellEnquiry->mobile, 'available_date' => $tractorSellEnquiry->available_date, 'comment' => $tractorSellEnquiry->comment, 'model' => ($tractorSellEnquiry->model == NULL ? "" : $tractorSellEnquiry->model), 'location' => $tractorSellEnquiry->location, 'other_city' => ($tractorSellEnquiry->other_city == NULL ? "" : $tractorSellEnquiry->other_city), 'what_type' => $tractorSellEnquiry->what_type, 'user_type' => $tractorSellEnquiry->user_type, 'is_edit' => "".$tractorSellEnquiry->is_edit);
-
                     $status_code = '1';
                     $message = 'Tractor Rent history';
-                    $json = array('status_code' => $status_code, 'message' => $message, 'tractorRentData' => $tractorRentData);
+                    $json = array('status_code' => $status_code, 'message' => $message, 'id' => "".$tractorSellEnquiry->id, 'name' => $tractorSellEnquiry->name, 'mobile' => $tractorSellEnquiry->mobile, 'available_date' => $tractorSellEnquiry->available_date, 'comment' => $tractorSellEnquiry->comment, 'model' => ($tractorSellEnquiry->model == NULL ? "" : $tractorSellEnquiry->model), 'location' => $tractorSellEnquiry->location, 'other_city' => ($tractorSellEnquiry->other_city == NULL ? "" : $tractorSellEnquiry->other_city), 'what_type' => $tractorSellEnquiry->what_type, 'user_type' => $tractorSellEnquiry->user_type, 'is_edit' => "".$tractorSellEnquiry->is_edit);
                 }
                 else
                 {
