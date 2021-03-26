c45328dac99a08f167e61237e21e50ab6130ba1e
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-26 13:03:30 +0530

    cs

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -29,6 +29,54 @@ class AdminController extends Controller
             trans('backpack::base.dashboard') => false,
         ];
 
+        $chart1 = \Chart::title([
+            'text' => 'Voting ballon d`or 2018',
+        ])
+        ->chart([
+            'type'     => 'line', // pie , columnt ect
+            'renderTo' => 'chart1', // render the chart into your div with id
+        ])
+        ->subtitle([
+            'text' => 'This Subtitle',
+        ])
+        ->colors([
+            '#0c2959'
+        ])
+        ->xaxis([
+            'categories' => [
+                'Alex Turner',
+                'Julian Casablancas',
+                'Bambang Pamungkas',
+                'Mbah Surip',
+            ],
+            'labels'     => [
+                'rotation'  => 15,
+                'align'     => 'top',
+                'formatter' => 'startJs:function(){return this.value + " (Footbal Player)"}:endJs', 
+                // use 'startJs:yourjavasscripthere:endJs'
+            ],
+        ])
+        ->yaxis([
+            'text' => 'This Y Axis',
+        ])
+        ->legend([
+            'layout'        => 'vertikal',
+            'align'         => 'right',
+            'verticalAlign' => 'middle',
+        ])
+        ->series(
+            [
+                [
+                    'name'  => 'Voting',
+                    'data'  => [43934, 52503, 57177, 69658],
+                    // 'color' => '#0c2959',
+                ],
+            ]
+        )
+        ->display();
+
+        $this->data['chart1'] = $chart1;
+
         return view('admin.dashboard', $this->data);
     }
 
