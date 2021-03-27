ad6c811b635f3b7492201f1b59618f8e88a30497
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-27 17:58:11 +0530

    Update UserNotificationController.php

--- app/Http/Controllers/Admin/UserNotificationController.php
+++ app/Http/Controllers/Admin/UserNotificationController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Requests\NotificationRequest;
+use Illuminate\Http\Request;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 
