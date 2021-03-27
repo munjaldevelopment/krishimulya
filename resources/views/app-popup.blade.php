367b3516109cf2c1ce7cb3b984ebef45c782f35b
Munjal Mayank <munjaldevelopment@gmail.com>
2021-03-27 12:55:10 +0530

    Update app-popup.blade.php

--- resources/views/app-popup.blade.php
+++ resources/views/app-popup.blade.php
@@ -12,11 +12,21 @@
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="col-sm-12">
+                <div class="col-sm-12 navbar-text">
                     <img src="{{ URL::to("/")."/".$sliderList->image }}" class="img-fluid">
 
                     <h3>{{ $sliderList->short_description }}</h3>
