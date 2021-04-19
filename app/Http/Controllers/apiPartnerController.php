f55d90a5d12651abb39b4a911546359fb6f18792
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-19 18:11:19 +0530

    Update apiPartnerController.php

--- app/Http/Controllers/apiPartnerController.php
+++ app/Http/Controllers/apiPartnerController.php
@@ -952,14 +952,14 @@ class apiPartnerController extends Controller
             $customer = DB::table('vendors')->where('id', $customer_id)->where('is_onboard', '=', '1')->first();
             if($customer){
                 $custname = $customer->name;
-                $custcrn = ($customer->crn == NULL ? "" : $customer->crn);
+                //$custcrn = ($customer->crn == NULL ? "" : $customer->crn);
 
                 // update app version in
                 $date = date('Y-m-d H:i:s');
                 DB::table('customers')->where('id', '=', $customer_id)->update(['app_version' => $app_version, 'updated_at' => $date]);
             } else {
                 $custname = "Guest";
-                $custcrn = "";
+                //$custcrn = "";
             }
 
             $sliderArr = array();
