7a98f98476d5bfc27cfb17efa54ebc8881e39765
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-21 19:10:27 +0530

    Update apiPartnerController.php

--- app/Http/Controllers/apiPartnerController.php
+++ app/Http/Controllers/apiPartnerController.php
@@ -654,14 +654,12 @@ class apiPartnerController extends Controller
                     // Get Value
                     $vendorData1 = \DB::table($table_name_vendor)->where('id', $lead_id)->first();
 
-                    //echo $table_name.",".
-                    echo $table_name_id.",".$table_name_vendor_id;
-                    
-                    dd($vendorData1);
+                    //echo $table_name_id.",".$table_name_vendor_id;
+                    //dd($vendorData1);
                     
                     \DB::table($table_name_vendor_history)->insert([
-                        $table_name_id => $vendorData1->$table_name_id, 
-                        $table_name_vendor_id => $vendorData1->$table_name_vendor_id,
+                        "".$table_name_id => $vendorData1->$table_name_id, 
+                        "".$table_name_vendor_id => $vendorData1->$table_name_vendor_id,
                         'vendor_id' => $partner_id, 
                         'test_status' => $test_status,
                         'status_time' => date('Y-m-d H:i:s'),
