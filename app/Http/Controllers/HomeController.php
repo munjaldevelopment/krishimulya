bfc6e5767071cd678e38f5a75f231daece814314
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-21 14:26:49 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,26 @@ class HomeController extends Controller
         foreach($vendors as $vendor)
         {
             $vendorAssign = \DB::table('vendor_vendor_assign')->leftJoin('vendor_services', 'vendor_vendor_assign.vendor_service_id', '=', 'vendor_services.id')->where('vendor_id', $vendor->id)->get();
-            dd($vendorAssign);
+
+            if($vendorAssign)
+            {
+                foreach($vendorAssign as $assignData)
+                {
+                    $table_name = $vendorAssign->table_name;
+                    $table_name_vendor = $vendorAssign->table_name."_vendor";
+
+                    $vendorData = \DB::table($table_name)->get();
+
+                    if($vendorData)
+                    {
+                        foreach($vendorData as $vendorRow)
+                        {
+                            $id = $vendorRow->id;
+                            echo $id; exit;
+                        }
+                    }
+                }
+            }
         }
     }
 }
