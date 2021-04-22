2f7f880a273e4625ba08bc56c8a1d773389d0927
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-22 15:39:55 +0530

    Update apiSoilController.php

--- app/Http/Controllers/apiSoilController.php
+++ app/Http/Controllers/apiSoilController.php
@@ -126,7 +126,7 @@ class apiSoilController extends Controller
           CURLOPT_FOLLOWLOCATION => true,
           CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
           CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS => '{"query":"mutation CreateFarmerMutation($createFarmerFarmer: FarmerInput!) { createFarmer(farmer: $createFarmerFarmer) { id latitude longitude name address phone username createdAt updatedAt }}","variables":{"createFarmerFarmer":{"name":"True Friend83","address":"TestAddress","phone":"+919999999999","latitude":12.566465,"longitude":34.453666,"username":"truefriend831001"}}}',
+          CURLOPT_POSTFIELDS => '{"query":"mutation CreateFarmerMutation($createFarmerFarmer: FarmerInput!) { createFarmer(farmer: $createFarmerFarmer) { id latitude longitude name address phone username createdAt updatedAt }}","variables":{"createFarmerFarmer":{"name":"True Friend83","address":"TestAddress","phone":"+919999999999","latitude":12.566465,"longitude":34.453666,"username":"truefriend845"}}}',
           CURLOPT_HTTPHEADER => array(
             'Authorization: Bearer '.SOILTEST_TOKEN,
             'Content-Type: application/json'
@@ -137,13 +137,15 @@ class apiSoilController extends Controller
 
         curl_close($curl);
 
-        if(isset($response['data']['createFarmer']['id']))
+        $result = json_decode($response, 1);
+
+        if(isset($result['data']['createFarmer']['id']))
         {
-        	echo $response['data']['createFarmer']['id'];
+        	echo $result['data']['createFarmer']['id'];
         }
         else
         {
-        	echo '<pre>'; print_r($response); exit;	
+        	echo '<pre>'; print_r($result); exit;	
         }
     }
 
