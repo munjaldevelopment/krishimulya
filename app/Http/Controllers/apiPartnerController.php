c3a7f38846f06190e2f4a0035d85ef493b03ff16
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-22 10:19:49 +0530

    Update apiPartnerController.php

--- app/Http/Controllers/apiPartnerController.php
+++ app/Http/Controllers/apiPartnerController.php
@@ -990,9 +990,9 @@ class apiPartnerController extends Controller
                     $assignService[] = array('service_code' => $value->service_code, 'service_color' => $value->service_color, 'image' => $baseUrl."/".$value->image, 'name' => $categoryName->$language, 'stats' => "".$stats);
                 }
 
-                $assignService[] = array('service_code' => 'all-Leads', 'service_color' => $value->service_color, 'image' => '', 'stats' => "".$stats_total);
+                $assignService[] = array('service_code' => 'all-Leads', 'service_color' => $value->service_color, 'image' => $baseUrl."/".$value->image, 'stats' => "".$stats_total);
 
-                $assignService[] = array('service_code' => 'pending-leads', 'service_color' => $value->service_color, 'image' => '', 'stats' => "".$stats_total);
+                $assignService[] = array('service_code' => 'pending-leads', 'service_color' => $value->service_color, 'image' => $baseUrl."/".$value->image, 'stats' => "".$stats_total);
 
 
                 asort($assignService);
