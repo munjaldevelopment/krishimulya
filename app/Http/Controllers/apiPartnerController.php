16ecd0ae65c83c0ad3bab77be4399212184bad92
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-22 10:22:22 +0530

    Update apiPartnerController.php

--- app/Http/Controllers/apiPartnerController.php
+++ app/Http/Controllers/apiPartnerController.php
@@ -995,7 +995,7 @@ class apiPartnerController extends Controller
                 $assignService[] = array('service_code' => 'pending-leads', 'service_color' => $value->service_color, 'image' => $baseUrl."/".$value->image, 'stats' => "".$stats_total);
 
 
-                //arsort($assignService);
+                array_reverse($assignService);
 
                 $pincode = $partner->pincode;
 
