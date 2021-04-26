8201874406c5b07cfe46855355bd6a6a1fd26124
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-26 10:28:52 +0530

    Update fcm.php

--- config/fcm.php
+++ config/fcm.php
@@ -7,6 +7,8 @@ return [
     'http' => [
         'server_key' => env('FCM_SERVER_KEY', 'Your FCM server key'),
         'sender_id' => env('FCM_SENDER_ID', 'Your sender id'),
+        'server_key_client' => env('FCM_SERVER_KEY_CLIENT', 'Your FCM server key'),
+        'sender_id_client' => env('FCM_SENDER_ID_CLIENT', 'Your sender id'),
         'server_send_url' => 'https://fcm.googleapis.com/fcm/send',
         'server_group_url' => 'https://android.googleapis.com/gcm/notification',
         'timeout' => 30.0, // in second
