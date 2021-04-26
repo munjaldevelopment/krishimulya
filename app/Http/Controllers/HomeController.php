0d0f9e0b36964cb7bdfcd4e3be1c3eeb58bbc1ef
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-26 10:38:41 +0530

    cs

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,6 +47,7 @@ class HomeController extends Controller
                 $dataBuilder->addData(['title' => $title, 'content' => $message]);
                 
                 $option = $optionBuilder->build();
+                dd($option);
 
                 $notification = $notificationBuilder->build();
                 $data = $dataBuilder->build();
