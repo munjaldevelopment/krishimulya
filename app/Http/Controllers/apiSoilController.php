d165bcd0928f4e015049b46b7d59d30bb6bc0036
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-22 11:59:36 +0530

    Update apiSoilController.php

--- app/Http/Controllers/apiSoilController.php
+++ app/Http/Controllers/apiSoilController.php
@@ -202,6 +202,7 @@ class apiSoilController extends Controller
 	        else
 	        {
 	        	echo $cust_name1.">".$result['errors'][0]['message'].'<br />';
+	        	exit;
 	        	
 	        }
 		}
