cdbffdbf0404744666439575c114bba10755707f
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-22 12:08:37 +0530

    Update apiSoilController.php

--- app/Http/Controllers/apiSoilController.php
+++ app/Http/Controllers/apiSoilController.php
@@ -129,31 +129,40 @@ class apiSoilController extends Controller
     {
         Setting::AssignSetting();
 
-        $curl = curl_init();
+        $customers = \DB::table("customers")->whereNull('krishitantra_id')->skip(0)->take(10)->get();
 
-        curl_setopt_array($curl, array(
-          CURLOPT_URL => SOILTEST_URL,
-          CURLOPT_RETURNTRANSFER => true,
-          CURLOPT_ENCODING => '',
-          CURLOPT_MAXREDIRS => 10,
-          CURLOPT_TIMEOUT => 0,
-          CURLOPT_FOLLOWLOCATION => true,
-          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-          CURLOPT_CUSTOMREQUEST => 'POST',
-          CURLOPT_POSTFIELDS => '{"query":"query Query($getUsersOrganization: ID!, $getUsersUsername: String) { getUsers(organization: $getUsersOrganization, username: $getUsersUsername)  {     id     latitude     longitude     name     address     phone     email     username     createdAt     updatedAt }}","variables":{"getUsersOrganization":"6038ba9130a8cd001200110d","getUsersUsername":"TEST-abhishek-sharma"}}',
-          CURLOPT_HTTPHEADER => array(
-            'Authorization: Bearer '.SOILTEST_TOKEN,
-            'Content-Type: application/json'
-          ),
-        ));
+        foreach ($customers as $key => $row) {
 
-        $response = curl_exec($curl);
+        	$cust_id = $row->id;
+        	$cust_name = $row->name;
+        	$cust_name1 = str_replace(" ", "-", strtolower($row->name));
 
-        curl_close($curl);
+	        $curl = curl_init();
 
-        $result = json_decode($response, 1);
+	        curl_setopt_array($curl, array(
+	          CURLOPT_URL => SOILTEST_URL,
+	          CURLOPT_RETURNTRANSFER => true,
+	          CURLOPT_ENCODING => '',
+	          CURLOPT_MAXREDIRS => 10,
+	          CURLOPT_TIMEOUT => 0,
+	          CURLOPT_FOLLOWLOCATION => true,
+	          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	          CURLOPT_CUSTOMREQUEST => 'POST',
+	          CURLOPT_POSTFIELDS => '{"query":"query Query($getUsersOrganization: ID!, $getUsersUsername: String) { getUsers(organization: $getUsersOrganization, username: $getUsersUsername)  {     id     latitude     longitude     name     address     phone     email     username     createdAt     updatedAt }}","variables":{"getUsersOrganization":"6038ba9130a8cd001200110d","getUsersUsername":"KRISHITEST-'.$cust_name1.'"}}',
+	          CURLOPT_HTTPHEADER => array(
+	            'Authorization: Bearer '.SOILTEST_TOKEN,
+	            'Content-Type: application/json'
+	          ),
+	        ));
 
-        echo '<pre>'; print_r($result); exit;
+	        $response = curl_exec($curl);
+
+	        curl_close($curl);
+
+	        $result = json_decode($response, 1);
+
+	        echo '<pre>'; print_r($result); exit;
+	    }
     }
 
     public function soilCreateFarmer(Request $request)
