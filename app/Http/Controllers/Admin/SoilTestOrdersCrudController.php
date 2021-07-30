bf6e1d9c371da06fe2f0df090baf923dc5c8b332
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-30 22:12:14 +0530

    admin changes

--- app/Http/Controllers/Admin/SoilTestOrdersCrudController.php
+++ app/Http/Controllers/Admin/SoilTestOrdersCrudController.php
@@ -350,7 +350,7 @@ class SoilTestOrdersCrudController extends CrudController
             $.ajax({
                 url: "saveSoilTest",
                 type: "post",
-                data: {soil_test_id : '.$soil_test_id.', famer_name : $("tr.Table12 td.Table1_B2:nth-child(2)").html(), famer_code : $("tr.Table12 td.Table1_B2:nth-child(4)").html()},
+                data: {soil_test_id : '.$soil_test_id.', famer_name : $("tr:nth-child(2) td.Table1_B2:nth-child(2)").html(), famer_code : $("tr:nth-child(2) td.Table1_B2:nth-child(4)").html(), crop_grown : $("tr:nth-child(2) td.Table1_B2:nth-child(6)").html(), sample_number : $("tr:nth-child(2) td.Table1_B2:nth-child(8)").html()},
                 success:function(response) {
                 console.log(response);
                 }
