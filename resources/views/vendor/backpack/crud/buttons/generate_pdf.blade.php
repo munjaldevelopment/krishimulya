e23e735a32d93dc5f258e4542ea33dffed4b8742
Munjal Mayank <munjaldevelopment@gmail.com>
2021-07-31 17:03:46 +0530

    admin changes

--- resources/views/vendor/backpack/crud/buttons/generate_pdf.blade.php
+++ resources/views/vendor/backpack/crud/buttons/generate_pdf.blade.php
@@ -4,7 +4,11 @@
 		$isExists = DB::table('soil_test_order_data')->where('soil_test_order_id', $soil_test_order_id)->count();
 		if($isExists == 0):
 	@endphp
-	<a href="{{ backpack_url('download_soil_test') }}?soil_test_id={{ $entry->getKey() }}" target="_blank" class="btn btn-xs btn-success"><i class="fa fa-cloud"></i> Generate</a>
+	<a href="{{ backpack_url('download_soil_test') }}?soil_test_id={{ $entry->getKey() }}" target="_blank" class="btn btn-xs btn-info"><i class="fa fa-cloud"></i> Generate</a>
+	@php
+		else:
+	@endphp
+	<a href="javascript:;" class="btn btn-xs btn-success"><i class="fa fa-cloud"></i> Generated</a>
 	@php
 		endif;
 	@endphp
