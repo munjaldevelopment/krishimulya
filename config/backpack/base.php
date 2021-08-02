8fb8ef98cca6c21bb9b41340fd6e5ff4a928f379
Munjal Mayank <munjaldevelopment@gmail.com>
2021-08-02 12:15:31 +0530

    admin changes

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -13,7 +13,7 @@ return [
 
     // Date & Datetime Format Syntax: https://carbon.nesbot.com/docs/#api-localization
     'default_date_format'     => 'D MMM YYYY',
-    'default_datetime_format' => 'D M YYYY, hh:mm a',
+    'default_datetime_format' => 'D-M-YYYY hh:mm a',
 
     // Direction, according to language
     // (left-to-right vs right-to-left)
